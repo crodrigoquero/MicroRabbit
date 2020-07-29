e81a0adf8d0b78efa6c3527f787525f50bea9f6f
Carlos Rodrigo <crodrigoquero@gmail.com>
2020-07-29 18:13:23 +0100

    Added MVC UI Application

---
+++ MicroRabbit.MVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MicroRabbit.MVC.Models;
+using MicroRabbit.MVC.Services;
+using MicroRabbit.MVC.Models.DTO;
+
+namespace MicroRabbit.MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITransferService _transferService;
+
+
+        public HomeController(ITransferService transferService)
+        {
+            _transferService = transferService;
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Transfer(TransferViewModel model)
+        {
+            TransferDto transferDto = new TransferDto()
+            {
+                FromAccount = model.FromAccount,
+                ToAccount = model.ToAccount,
+                TransferAmout = model.TransferAmount
+            };
+
+            // transfer service was injected
+            await _transferService.Transfer(transferDto); // I've turned the functionality we need into a service, in order to such functionality to be reusable (and injectable)
+
+            return View("Index");
+        }
+
+    }
+}
